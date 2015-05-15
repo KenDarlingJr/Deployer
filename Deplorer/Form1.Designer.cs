33d417429e48df9238027b26458a42215d2c0ee9
Ken Darling <kdarling@pafcorp.com>
2015-05-15 16:29:52 -0400

    1st Commit

---
+++ Deplorer/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Deplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblCurrentTime = new System.Windows.Forms.Label();
+            this.currentTime = new System.Windows.Forms.Timer(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblTimeMatch = new System.Windows.Forms.Label();
+            this.lblSelectedTime = new System.Windows.Forms.Label();
+            this.btnSelectTime = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtDestPath = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtDestFile = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lblCurrentTime
+            // 
+            this.lblCurrentTime.AutoSize = true;
+            this.lblCurrentTime.Location = new System.Drawing.Point(12, 246);
+            this.lblCurrentTime.Name = "lblCurrentTime";
+            this.lblCurrentTime.Size = new System.Drawing.Size(67, 13);
+            this.lblCurrentTime.TabIndex = 0;
+            this.lblCurrentTime.Text = "Current Time";
+            // 
+            // currentTime
+            // 
+            this.currentTime.Tick += new System.EventHandler(this.currentTime_Tick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "hh:mm:ss";
+            this.dateTimePicker1.Location = new System.Drawing.Point(118, 92);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(319, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // lblTimeMatch
+            // 
+            this.lblTimeMatch.AutoSize = true;
+            this.lblTimeMatch.Location = new System.Drawing.Point(12, 304);
+            this.lblTimeMatch.Name = "lblTimeMatch";
+            this.lblTimeMatch.Size = new System.Drawing.Size(147, 13);
+            this.lblTimeMatch.TabIndex = 2;
+            this.lblTimeMatch.Text = "Selected Time = Current Time";
+            // 
+            // lblSelectedTime
+            // 
+            this.lblSelectedTime.AutoSize = true;
+            this.lblSelectedTime.Location = new System.Drawing.Point(12, 263);
+            this.lblSelectedTime.Name = "lblSelectedTime";
+            this.lblSelectedTime.Size = new System.Drawing.Size(82, 13);
+            this.lblSelectedTime.TabIndex = 3;
+            this.lblSelectedTime.Text = "lblSelectedTime";
+            // 
+            // btnSelectTime
+            // 
+            this.btnSelectTime.Location = new System.Drawing.Point(12, 93);
+            this.btnSelectTime.Name = "btnSelectTime";
+            this.btnSelectTime.Size = new System.Drawing.Size(100, 23);
+            this.btnSelectTime.TabIndex = 4;
+            this.btnSelectTime.Text = "button1";
+            this.btnSelectTime.UseVisualStyleBackColor = true;
+            this.btnSelectTime.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 200);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(118, 14);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(319, 20);
+            this.txtSource.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Source Path/File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Destination Path";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtDestPath
+            // 
+            this.txtDestPath.Location = new System.Drawing.Point(118, 40);
+            this.txtDestPath.Name = "txtDestPath";
+            this.txtDestPath.Size = new System.Drawing.Size(319, 20);
+            this.txtDestPath.TabIndex = 14;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Destination File";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // txtDestFile
+            // 
+            this.txtDestFile.Location = new System.Drawing.Point(118, 66);
+            this.txtDestFile.Name = "txtDestFile";
+            this.txtDestFile.Size = new System.Drawing.Size(319, 20);
+            this.txtDestFile.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 345);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtDestFile);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtDestPath);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnSelectTime);
+            this.Controls.Add(this.lblSelectedTime);
+            this.Controls.Add(this.lblTimeMatch);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblCurrentTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCurrentTime;
+        private System.Windows.Forms.Timer currentTime;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblTimeMatch;
+        private System.Windows.Forms.Label lblSelectedTime;
+        private System.Windows.Forms.Button btnSelectTime;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtDestPath;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtDestFile;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Deplorer/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Deplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void currentTime_Tick(object sender, EventArgs e)
+        {
+            lblCurrentTime.Text = DateTime.Now.ToLongTimeString();
+            if (checkBox1.Checked)
+                checkTimes();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            currentTime.Enabled = true;
+            dateTimePicker1.Format = DateTimePickerFormat.Time;
+            dateTimePicker1.ShowUpDown = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblSelectedTime.Text = dateTimePicker1.Value.ToLongTimeString();
+            saveItem();
+            //checkBox1.Checked = true;
+
+        }
+
+        private void checkTimes() 
+        {
+            DateTime selTime = DateTime.Parse(lblSelectedTime.Text, System.Globalization.CultureInfo.CurrentCulture);
+
+            int result = DateTime.Compare(DateTime.Now, selTime);
+
+            string strSource = "C:\\Staging\\Receiving Insp FE  1.4.accdb";
+            string strDest = "C:\\Staging\\Receiving Insp FE.accdb";
+
+            if (result == 1)
+            {
+                //Trigger the event here
+                File.Copy(strSource, strDest, true);
+                lblTimeMatch.Text = "Trigger Event";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fileName;
+            DialogResult result = openFileDialog1.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+                txtSource.Text = fileName;
+                //txtDestFile.Text = fileName.LastIndexOf("\\").ToString();
+                txtDestFile.Text = fileName.Substring(fileName.LastIndexOf("\\")+1);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtDestPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void saveItem()
+        {
+
+            if (File.Exists("C:\\Staging\\myFile.xml") == false)
+            {
+                XmlWriterSettings xmlSettings = new XmlWriterSettings();
+                xmlSettings.Indent = true;
+                xmlSettings.NewLineOnAttributes = true;
+
+                using (XmlWriter writer = XmlWriter.Create("C:\\Staging\\myFile.xml",xmlSettings))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Items");
+
+                    writer.WriteStartElement("Item");
+                    writer.WriteElementString("Source", txtSource.Text);
+                    writer.WriteElementString("Destination", txtDestPath.Text + "\\" + txtDestFile.Text);
+                    writer.WriteElementString("Time", lblSelectedTime.Text);
+                    writer.WriteEndElement();
+
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                }
+            }
+            else
+            {
+                XDocument xDoc = XDocument.Load("C:\\Staging\\myFile.xml");
+                XElement root = xDoc.Element("Items");
+                IEnumerable<XElement> rows = root.Descendants("Item");
+                XElement firstRow = rows.First();
+                firstRow.AddBeforeSelf(
+                    new XElement("Item", 
+                    new XElement("Source", txtSource.Text),
+                    new XElement("Destination", txtDestPath.Text + "\\" + txtDestFile.Text),
+                    new XElement("Time", lblSelectedTime.Text)));
+
+                xDoc.Save("C:\\Staging\\myFile.xml");
+            }
+
+        }
+    }
+}
